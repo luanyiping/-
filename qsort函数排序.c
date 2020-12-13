ab233a89c1f499fa075015a9c8b33aa32e1aa95d
luanyiping <3349244951@qq.com>
2020-12-13 13:09:06 +0800

    题目描述 期中考试开始了，大家都想取得好成绩，争夺前五名。从键盘输入n个学生成绩（不超过40个），输出每组排在前五高的成绩。
    输入描述:
    两行，第一行输入一个整数，表示n个学生（>=5），第二行输入n个学生成绩（整数表示，范围0~100），用空格分隔。
    
    输出描述:
    一行，输出成绩最高的前五个，用空格分隔。

---
+++ qsort函数排序.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+int compare(const void* p1, const void* p2)
+{
+	return((*(int*)p2) - (*(int*)p1));
+}
+int main()
+{
+	int a[40], n, i;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+	qsort(a, n, 4, compare);
+	for (i = 0; i < 5; i++)
+	{
+		printf("%d ", a[i]);
+	}
+}
+
+
+
+
