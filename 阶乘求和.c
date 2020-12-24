eb710a1f7dab167f0a65224b3c376e1ec96469ae
luanyiping <3349244951@qq.com>
2020-12-24 21:22:28 +0800

    阶乘求和

---
+++ 阶乘求和.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a = 1, n, i = 1;
+	long long sum = 0;
+	scanf_s("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		a = a * i;
+		sum = sum + a;
+	}
+	printf("%lld", sum);
+
+
+}
