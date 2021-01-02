cf69f9d7498cc5657b9cf8dba0b1161c49d2fe72
luanyiping <3349244951@qq.com>
2021-01-02 23:34:08 +0800

    strcpy函数模拟实现

--- strlen模拟实现(一).cpp
+++ strlen模拟实现(一).cpp
@@ -1,30 +1,10 @@
 #include<iostream>
 #include<cassert>
 using namespace std;
-int my_strlen(const char* str)
-{
-	int count = 0;
-	assert(str != NULL);
-	while (*str != '\0')
-	{
-		count++;
-		str++;
-	}
-	return count;
-}
 int main()
 {
-	int len = my_strlen("abcdef");
-	//char arr[] = { 'a','b','c','d' };
-	//  int len = my_strlen(arr);
-	printf("%d\n", len);
-	if (strlen("abc") - strlen("abcdef") > 0)
-	{
-		printf("hehe\n");
-	}
-	else
-	{
-		printf("haha\n");
-	}
+	char arr1[] = "abcdefghi";
+	char arr2[] = "bit";
+	strcmp(arr1, arr2);
 }
 
